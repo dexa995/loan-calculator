32efb50d1c0871ab7dff2ee293ea9c48e116cdf0
dejan_krstic995 <dejan.krstic@leanpay.com>
2025-07-08 22:34:55 +0200

    Used @MockitoBean instead of @MockBean in CashFlowControllerWebMvcTest.

--- src/test/java/com/leanpay/loancalculator/unit/api/CashFlowControllerWebMvcTest.java
+++ src/test/java/com/leanpay/loancalculator/unit/api/CashFlowControllerWebMvcTest.java
@@ -17,8 +17,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -38,8 +38,7 @@ public class CashFlowControllerWebMvcTest {
 	@Autowired
 	private ObjectMapper objectMapper;
 
-	@SuppressWarnings("removal")
-	@MockBean
+	@MockitoBean
 	private CashFlowService cashFlowService;
 
 	@Test
